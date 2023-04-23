5e3094a1cd4df46e1570af3878f2f1fbad7923cd
AmalSN <amalkrish12@gmail.com>
2023-04-23 14:35:14 +0530

    extract

--- FRAS/extract_pic.py
+++ FRAS/extract_pic.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import os
 import time
+import cv2
 
 def extract_pic():
     url = "http://192.168.4.1/"
@@ -15,5 +16,10 @@ def extract_pic():
         driver.get_screenshot_as_file(dir+"/"+str(i)+".png")
         time.sleep(0.1)
 
+    for f in os.listdir(dir):
+        img = cv2.imread(os.path.join(dir,f))
+        img = img[10:250,10:400]
+        img = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
+        cv2.imwrite(os.path.join(dir,f),img)
 
 extract_pic()
